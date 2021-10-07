f4c28e1f929128347f2095e723dd39b83cc624df
robert085 <rkunc9398@gmail.com>
2021-10-07 11:08:07 +0200

    Add new code

--- exercise.js
+++ exercise.js
@@ -3,25 +3,33 @@
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
 console.log(typeof secretNumber, secretNumber);
 document.querySelector('.number').textContent = secretNumber;
+let message = document.querySelector('.message');
 
 let score = 20;
+let highScore = 0;
 
 document.querySelector('.check').addEventListener('click', function () {
   let guess = Number(document.querySelector('.guess').value);
 
   if (guess === secretNumber) {
-    document.querySelector('.message').textContent = '👍 Correct Number!';
+    message.textContent = '👍 Correct Number!';
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector('.highscore').textContent = highScore;
+    }
+  } else if (score > 1) {
+    if (guess > secretNumber) {
+      //   TOO HIGH
+      message.textContent = '😆 Too high!';
+      score--;
+      document.querySelector('.score').textContent = score;
 
-    //   TOO HIGH
-  } else if (guess > secretNumber) {
-    document.querySelector('.message').textContent = '😆 Too high!';
-    score--;
-    document.querySelector('.score').textContent = score;
-
-    // TOO LOW
-  } else if (guess < secretNumber) {
-    document.querySelector('.message').textContent = '😆 Too low!';
-    score--;
-    document.querySelector('.score').textContent = score;
-  }
+      // TOO LOW
+    } else if (guess < secretNumber) {
+      message.textContent = '😆 Too low!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    }
+  } else message.textContent = '😁 Game over!';
 });
