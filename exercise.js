707b40ca6e6246c679d93248a2a0d0e2cac299fe
robert085 <rkunc9398@gmail.com>
2021-10-07 19:32:06 +0200

    Game finished

--- exercise.js
+++ exercise.js
@@ -5,20 +5,22 @@ const secretNumber = Math.trunc(Math.random() * 20) + 1;
 let number = document.querySelector('.number');
 let message = document.querySelector('.message');
 let currScore = document.querySelector('.score');
-// let guess = document.querySelector('.guess');
+document.querySelector('h1').classList.add('guessed');
+let guessed = document.querySelector('.guessed');
 
 let score = 20;
 let highScore = 0;
 
-// SEECRET NUMBER
-number.textContent = secretNumber;
 // CHECK - output
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
 
-  console.log(guess);
-
-  if (guess === secretNumber) {
+  if (!guess) {
+    message.textContent = '❌ No number';
+  } else if (guess === secretNumber) {
+    // SEECRET NUMBER
+    number.textContent = secretNumber;
+    guessed.textContent = '💥 Guessed! 💥';
     message.textContent = '👍 Correct Number!';
     document.querySelector('body').style.backgroundColor = '#60b347';
     if (score > highScore) {
@@ -42,7 +44,11 @@ document.querySelector('.check').addEventListener('click', function () {
 });
 
 document.querySelector('.again').addEventListener('click', function () {
+  const secretNumber = Math.trunc(Math.random() * 20) + 1;
+  number.textContent = '?';
   document.querySelector('body').style.backgroundColor = '#222';
   currScore.textContent = 20;
   document.querySelector('.guess').value = '';
+  guessed.textContent = 'Guess My Number';
+  message.textContent = 'Start guessing...';
 });
